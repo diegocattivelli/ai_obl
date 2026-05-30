64ea68b44a802cfbdda8c9070f94f503c91ba553
Diego <dcattivellib@gmail.com>
2026-05-30 10:05:27 -0300

    first commit

---
+++ MountainCarContinuous/q_learning_agent.py
@@ -0,0 +1,17 @@
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class QLearningAgent:
+    def __init__(self):
+        pass
+
+    def next_action(self, obs):
+        pass
+
+    def train_agent(self, env, episodes=1000, epsilon=.9, gamma=.9, alpha=.99):
+        pass
+
+    def test_agent(self, env, episodes=10):
+        pass
